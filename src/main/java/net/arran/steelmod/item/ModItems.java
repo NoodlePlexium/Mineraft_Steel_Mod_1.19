16eeffb80f851dcd818a021d98949cd4b8511965
NoodlePlexium <arran.ns@icloud.com>
2022-11-07 22:51:00 +0000

    Second Commit

--- src/main/java/net/arran/steelmod/item/ModItems.java
+++ src/main/java/net/arran/steelmod/item/ModItems.java
@@ -3,6 +3,10 @@ package net.arran.steelmod.item;
 import net.arran.steelmod.SteelMod;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,10 +19,19 @@ public class ModItems {
     public static final RegistryObject<Item> STEEL_INGOT = ITEMS.register("steel_ingot",
             ()-> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
 
-    public static final RegistryObject<Item> STEEL_SWORD = ITEMS.register("steel_sword",
-            ()-> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<SwordItem> STEEL_SWORD = ITEMS.register("steel_sword",
+            ()-> new SwordItem(Tiers.SteelTier,1,-2.4f,new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
+    }
+
+    public static class Tiers {
+        public static final Tier SteelTier = new ForgeTier(3,
+                512,
+                1f,
+                5,
+                350,
+                null, ()-> Ingredient.of(ModItems.STEEL_INGOT.get()));
     }
 }
